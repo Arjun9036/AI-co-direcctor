4dd4106dc33a5b18cc08249bf21da66e11fdcb55
Arjun Goyal <arjun.22bce9036@vitapstudent.ac.in>
2026-02-14 09:25:36 +0530

    fix: removed unused variables causing CI failure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
